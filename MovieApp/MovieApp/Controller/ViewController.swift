4383856f5f834d4b9847e280d03c8f524c8f0f46
ofeliacode <dir3ktt@yandex.ru>
2020-11-28 17:12:48 +0300

    release

--- MovieApp/MovieApp/Controller/ViewController.swift
+++ MovieApp/MovieApp/Controller/ViewController.swift
@@ -11,15 +11,15 @@ import RealmSwift
 
 import Foundation
 class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
- var limit:Int = 3
-   
+    var limit:Int = 3
+    
     var collectionView : UICollectionView?
     var movieModel = MovieViewModel()
     let searchController = UISearchController(searchResultsController: nil)
     var page = 1
     var movieManager = MovieManager()
-       
-
+    
+    
     private func showSearchBar() {
         navigationItem.searchController = searchController
         searchController.searchBar.delegate = self
@@ -49,10 +49,10 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         self.collectionView = collectionView
         collectionView.alwaysBounceVertical = false
     }
-
+    
     private func loadMoviesData() {
         movieModel.fetchMoviesData { [weak self] in
-        self?.collectionView?.reloadData()
+            self?.collectionView?.reloadData()
         }
     }
     func fetchMoviesFromRealmDB() {
@@ -62,9 +62,9 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       // realm.beginWrite()
-       // realm.delete(realm.objects(Movie.self))
-       // try! realm.commitWrite()
+        // realm.beginWrite()
+        // realm.delete(realm.objects(Movie.self))
+        // try! realm.commitWrite()
         fetchMoviesFromRealmDB()
         collectionView?.reloadData()
         showSearchBar()
@@ -75,12 +75,12 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         title = "Movie app"
     }
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//        let count = movieModel.numberOfRowInSections(section: section)
-//        if count <= 100 {
-//            return count
-//        } else {
-//            return 100
-//        }
+        //        let count = movieModel.numberOfRowInSections(section: section)
+        //        if count <= 100 {
+        //            return count
+        //        } else {
+        //            return 100
+        //        }
         
         return  min(movieModel.numberOfRowInSections(section: section), 100)
     }
@@ -92,43 +92,42 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         return cell
     }
     
-  func  getrequest() {
-    URLSession.shared.dataTask(with: URL(string:
-    "https://api.themoviedb.org/3/movie/popular?api_key=dfc41d3d13bc64503f9270485fa8746f&page=\(page)")!,
-       completionHandler: { [weak self] data, response, error in
-          guard let data = data, error == nil else {
-          return
-        }
-        var result: MoviesData?
-        do {
-         result = try JSONDecoder().decode(MoviesData.self, from: data)
-        }
-         catch {
-          print("error")
-        }
-         guard let finalResult = result else {
-          return
-        }
-        let newMovies = finalResult.results
-         self?.movieModel.movies.append(contentsOf: newMovies)
-        self?.page += 1
+    func  getrequest() {
+        URLSession.shared.dataTask(with: URL(string:
+                                                "https://api.themoviedb.org/3/movie/popular?api_key=dfc41d3d13bc64503f9270485fa8746f&page=\(page)")!,
+                                   completionHandler: { [weak self] data, response, error in
+                                    guard let data = data, error == nil else {
+                                        return
+                                    }
+                                    var result: MoviesData?
+                                    do {
+                                        result = try JSONDecoder().decode(MoviesData.self, from: data)
+                                    }
+                                    catch {
+                                        print("error")
+                                    }
+                                    guard let finalResult = result else {
+                                        return
+                                    }
+                                    let newMovies = finalResult.results
+                                    self?.movieModel.movies.append(contentsOf: newMovies)
+                                    self?.page += 1
+                                    
+                                    DispatchQueue.main.async {
+                                        let realm = try! Realm()
+                                        realm.beginWrite()
+                                        realm.add((self?.movieModel.movies)!)
+                                        try! realm.commitWrite()
+                                        self?.collectionView?.reloadData()
+                                    }
+                                   }).resume()
         
-         DispatchQueue.main.async {
-            let realm = try! Realm()
-            realm.beginWrite()
-            realm.add((self?.movieModel.movies)!)
-            try! realm.commitWrite()
-            self?.collectionView?.reloadData()
-         }
-        }).resume()
-   
-}
+    }
     
     
-    
- func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
-    if indexPath.row == movieModel.movies.count - 1 &&  movieModel.movies.count <= 60 {
-        getrequest()
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        if indexPath.row == movieModel.movies.count - 1 &&  movieModel.movies.count <= 60 {
+            getrequest()
         }
     }
 }
@@ -139,28 +138,28 @@ extension ViewController: UISearchBarDelegate {
         page = 1
         
         URLSession.shared.dataTask(with: URL(string: "https://api.themoviedb.org/3/search/movie?api_key=dfc41d3d13bc64503f9270485fa8746f&query=\(searchText)&page=\(page)")!,
-        completionHandler: { [weak self] data, response, error in
-    guard let data = data, error == nil else {
-        return
-    }
-    var result: MoviesData?
-    do {
-        result = try JSONDecoder().decode(MoviesData.self, from: data)
-    }
-    catch {
-        print("error")
-    }
-    guard let finalResult = result else {
-        return
-    }
-    let newMovies = finalResult.results
-    self?.movieModel.movies = newMovies
-    DispatchQueue.main.async {
-        self?.collectionView?.reloadData()
-    }
-   }).resume()
+                                   completionHandler: { [weak self] data, response, error in
+                                    guard let data = data, error == nil else {
+                                        return
+                                    }
+                                    var result: MoviesData?
+                                    do {
+                                        result = try JSONDecoder().decode(MoviesData.self, from: data)
+                                    }
+                                    catch {
+                                        print("error")
+                                    }
+                                    guard let finalResult = result else {
+                                        return
+                                    }
+                                    let newMovies = finalResult.results
+                                    self?.movieModel.movies = newMovies
+                                    DispatchQueue.main.async {
+                                        self?.collectionView?.reloadData()
+                                    }
+                                   }).resume()
         
-  }
+    }
 }
 
 extension ViewController: UICollectionViewDelegateFlowLayout {
